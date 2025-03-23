b075612a21a2c9087cc535290a96e7b22ca99f36
Hemanth <kusumahemanthkumar02@gmail.com>
2025-03-23 19:46:09 +0530

    March 23

--- dataproc/config/config.py
+++ dataproc/config/config.py
@@ -6,7 +6,8 @@ load_dotenv()
 class Config:
     """Configuration settings for the application."""
     # Astra DB credentials
-    ASTRA_DB_SECURE_BUNDLE_PATH = 'C:\\Users\\meeth\\OneDrive\\Documents\\rc_pai_batch\\mar 20\\secure-connect-rc-pai-test.zip'
+    #ASTRA_DB_SECURE_BUNDLE_PATH = 'C:\\Users\\meeth\\OneDrive\\Documents\\rc_pai_batch\\mar 20\\secure-connect-rc-pai-test.zip'
+    ASTRA_DB_SECURE_BUNDLE_PATH = '/app/secret_bundle/secure-connect-rc-pai-test.zip'
     ASTRA_DB_CLIENT_SECRET = "O8uglTzSG91,i-W52jMco+IpZWJTvzAyM1StnrgrtQwJdnCB-ein,GUZj_+yyHcYGqxNsr7OsM+iIdXrZRnDiU--f1ZPyYTm.FA+Dcwwa9vZtpsjuhG5ri6ok-CbTEWM"
     ASTRA_DB_CLIENT_ID = "uBNatRssAyFjivuYlOnHRmEH"
     ASTRA_DB_KEYSPACE='card_statement'
